c7d5b9a8822d49907f55504575efb179e4caf2b6
Tony Kaku <zym@youyueshi.com>
2016-06-17 13:41:30 +0800

    目录功能优化

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import React, {PropTypes} from 'react'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {Menu, Icon, notification} from 'antd'
-import {updateNavPath} from '../actions/menu'
+import {updateNavPath,getAllMenu} from '../actions/menu'
 import {Link} from 'react-router';
 
 const SubMenu = Menu.SubMenu
@@ -40,24 +40,95 @@ class Sidebar extends React.Component {
     }
 
     componentWillMount() {
-        const {menuErrors, items} = this.props;
-        if (menuErrors !== null){
-            console.log('menuErrors2==='+menuErrors)
-            this.context.router.replace('/login');
-
-            notification.error({
-                message: '加载遇到问题',
-                description: menuErrors
-            });
-        }
+        this.props.getAllMenu()
+        this.setState({
+            subpath : [''],
+            menupath : [''],
+            menuhead : 0,
+            location: this.props.location.pathname,
+            count: 0
+        })
     }
 
     componentDidUpdate() {
+        if(this.state.location != this.props.location.pathname || this.state.count == 0) {
+            var tmparr = this.props.items.map((item) => {
+                return (
+                    item.childMenus.map((node) => {
+                        return (
+                            node.childMenus.map((third) => {
+                                if (third.childMenus.length > 0) {
+                                    return (
+                                        third.childMenus.map((fourth) => {
+                                          return (
+                                              {routeUrl: fourth.routeUrl, menupath: node.id, subpath: 'sub'+item.id, thirdpath: 'thd'+third.id, fourthpath: 'frh'+fourth.id}
+                                          )
+                                        })
+                                    )
+                                } else {
+                                    return (
+                                        {routeUrl: third.routeUrl, menupath: node.id, subpath: 'sub'+item.id, thirdpath: 'thd'+third.id}
+                                    )
+                                }
+                            })
+                        )
+                    })
+                )
+            })
 
+            var cleanarr = []
+            for(let i=0 ; i < tmparr.length ; i++){
+                for (let j=0 ; j < tmparr[i].length; j++){
+                    for (let k=0 ; k < tmparr[i][j].length; k++) {
+                            cleanarr.push(tmparr[i][j][k])
+                    }
+                }
+            }
+
+            var tmppath = cleanarr.filter(item => item.routeUrl == this.props.location.pathname)
+            if (tmppath[0]){
+                this.setState({
+                    subpath : [tmppath[0].thirdpath],
+                    menupath : [tmppath[0].thirdpath],
+                    menuhead : tmppath[0].menupath,
+                    location: this.props.location.pathname,
+                    count: 1
+                })
+            } else {
+                var cleanerarr = []
+                for (let i=0; i < cleanarr.length; i++) {
+                    for (let j=0; j< cleanarr[i].length; j++) {
+                        cleanerarr.push(cleanarr[i][j])
+                    }
+                }
+                var tmppath2 = cleanerarr.filter(item => item.routeUrl == this.props.location.pathname)
+                if (tmppath2[0]) {
+                    this.setState({
+                        subpath : [tmppath2[0].thirdpath],
+                        menupath : [tmppath2[0].fourthpath],
+                        menuhead : tmppath2[0].menupath,
+                        location: this.props.location.pathname,
+                        count: 1
+                    })
+                }
+            }
+            console.log(tmppath[0] ? JSON.stringify(tmppath[0]) : JSON.stringify(tmppath2[0]));
+        }
     }
 
     menuClickHandle(item) {
         this.props.updateNavPath(item.keyPath, item.key);
+        this.setState({
+              subpath: [item.keyPath[0]],
+              menupath: [item.key]
+        })
+    }
+
+    titleClickHandle(eventKey, domEvent) {
+        console.log(eventKey.key);
+        this.setState({
+              subpath: [eventKey.key]
+        })
     }
 
     render() {
@@ -65,20 +136,6 @@ class Sidebar extends React.Component {
         var menupath = ''
         var subpath = ''
         var menuhead = ''
-
-        if (this.props.location.pathname === "/level2/level4"){
-            subpath = 'thd62',
-            menupath = 'frh63',
-            menuhead = 60
-        } else if (this.props.location.pathname === "/level22/level3"){
-            subpath = 'thd232',
-            menupath = '',
-            menuhead = 187
-        } else if (this.props.location.pathname === "/level2/level32"){
-            subpath = 'thd233',
-            menupath = '',
-            menuhead = 60
-        }
 
         // var availItems = [];
         // for (var i=0;i<items.length;i++)
@@ -90,32 +147,35 @@ class Sidebar extends React.Component {
 
         var menuset =[]
         for (let i=0; i<this.props.items.length; i++) {
-            var tmparrrr = this.props.items[i].childMenus.filter(node => node.id == menuhead)
+            var tmparrrr = this.props.items[i].childMenus.filter(node => node.id == this.state.menuhead)
             menuset.push(tmparrrr[0])
-            var menu = menuset[0].childMenus.map((node) => {
-                if (node.childMenus.length == 0) {
-                    return (
-                        <Menu.Item key={'thd'+node.id}>
-                            <Link to={node.routeUrl}><Icon type={node.iconType} />{node.name}</Link>
-                        </Menu.Item>
-                    )
-                } else {
-                    return (
-                        <SubMenu
-                            key={'thd'+node.id}
-                            title={<span><Icon type={node.iconType} />{node.name}</span>}
-                        >
-                            {node.childMenus.map((edon) => {
-                                return (
-                                    <Menu.Item key={'frh'+edon.id}>
-                                        <Link to={edon.routeUrl}><Icon type={edon.iconType} />{edon.name}</Link>
-                                    </Menu.Item>
-                                )
-                            })}
-                        </SubMenu>
-                    )
-                }
-            })
+            if(menuset[0]){
+                var menu = menuset[0].childMenus.map((node) => {
+                    if (node.childMenus.length == 0) {
+                        return (
+                            <Menu.Item key={'thd'+node.id}>
+                                <Link to={node.routeUrl}><Icon type={node.iconType} />{node.name}</Link>
+                            </Menu.Item>
+                        )
+                    } else {
+                        return (
+                            <SubMenu
+                                key={'thd'+node.id}
+                                title={<span><Icon type={node.iconType} />{node.name}</span>}
+                                onTitleClick={this.titleClickHandle.bind(this)}
+                            >
+                                {node.childMenus.map((edon) => {
+                                    return (
+                                        <Menu.Item key={'frh'+edon.id}>
+                                            <Link to={edon.routeUrl}><Icon type={edon.iconType} />{edon.name}</Link>
+                                        </Menu.Item>
+                                    )
+                                })}
+                            </SubMenu>
+                        )
+                    }
+                })
+            }
         }
 
         return (
@@ -123,8 +183,8 @@ class Sidebar extends React.Component {
                 <Menu
                     mode="inline" theme="dark"
                     onClick={this.menuClickHandle.bind(this)}
-                    defaultSelectedKeys={[menupath]}
-                    defaultOpenKeys={[subpath]}
+                    selectedKeys={this.state.menupath}
+                    openKeys={this.state.subpath}
                 >
                     {menu}
                 </Menu>
@@ -140,7 +200,6 @@ Sidebar.contextTypes = contextTypes;
 function mapStateToProps(state) {
 
     return {
-        menuErrors: state.menu.menuErrors,
         currentIndex: state.menu.currentIndex,
         items: state.menu.items
     }
@@ -148,7 +207,8 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        updateNavPath: bindActionCreators(updateNavPath, dispatch)
+        updateNavPath: bindActionCreators(updateNavPath, dispatch),
+        getAllMenu: bindActionCreators(getAllMenu, dispatch)
     }
 }
 
